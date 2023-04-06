7f64c441fc63b29e0a1b7593c0ff142345a135cd
Jooney <42194130+jhan756k@users.noreply.github.com>
2023-04-07 01:46:29 +0900

    apcsa

---
+++ APCSA Study/Unit1.java
@@ -0,0 +1,32 @@
+public class Unit1 {
+    public static void main(String[] args) {
+        /*
+        Constants - UPPERCASE_UNDERSCORE
+        final double PI;
+
+        Variable Naming
+        - No 1st number
+        - No special characters
+        - No reserved words
+        - boolean not bool
+
+        Operator Precedence
+        () * / % + - (L->R)
+
+        Arithmetic Exceptions - Divide by 0
+        
+        Assignment Operators - (R->L)
+        x=y=z=5; 
+
+        ++x 안나옴!
+
+        Casting 
+        (int)(number + 0.5) --> rounding
+        (int)(number - 0.5) --> rounding (negative)
+        
+        Overflow
+        Integer.MAX_VALUE = 2147483647
+        Integer.MIN_VALUE = -2147483648
+        */
+    }
+}
---
+++ APCSA Study/Unit2.java
@@ -0,0 +1,70 @@
+public class Unit2 {
+    public static void main(String[] args) {
+        /*
+        Objects and Classes
+
+        Class - blueprint for objects
+        Object - instance of a class
+
+        public Person(String nm, int ag, boolean ad) {
+            name = nm;
+            age = ag;
+            isAdult = ad;
+        }
+
+        Person p1 = new Person("John", 20, true);
+
+        Signature & Overloading
+        Which Constructor was used?
+
+        null - reference is not pointing to an object
+
+        Procedural Abstraction
+
+        Person p1; --> declaration
+        p1.talk(); --> Null Pointer Exception
+
+        immutable - unchangeable
+        String is immutable in Java
+
+        \ --> escape sequence
+        \", \\, \n --> ", \, new line
+
+        Java Quick Reference (JQR)
+
+        -- IndexOf --
+        import java.lang.String; --> not needed
+        "monkey".indexOf("e") --> 4
+        "monkey".indexOf("e", 5) --> -1
+        IndexOutOfBoundsException
+
+        java.lang is automatically imported
+
+        -- Substring -- 
+        "monkey".substring(2) --> "nkey"
+        "monkey".substring(2, 4) --> "nk"
+
+        -- Length --
+        "monkey".length() --> 6
+
+        -- Equals --
+        "monkey".equals("monkey") --> true
+
+        -- CompareTo --
+        https://mine-it-record.tistory.com/133
+        "monkey".compareTo("monkey") --> 0
+        "monkey".compareTo("monkeys") --> -1
+
+        Integer is a wrapper class, int is a primitive type
+
+        intValue(), doubleValue(), ... --> return the value of primitive data type of the wrapper class
+
+        Double d = new Double(3.141592);
+        double d2 = d.doubleValue();
+        System.out.println(d2);
+
+        AutoBoxing & Unboxing
+        
+        */
+    }
+}
