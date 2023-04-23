fa5cbbb77e52d536398b579d7220a8536305cfd9
Jooney <42194130+jhan756k@users.noreply.github.com>
2023-04-23 21:07:03 +0900

    unit5

--- APCSA Study/Unit5.java
+++ APCSA Study/Unit5.java
@@ -22,12 +22,35 @@ public class Unit5 {
         2. Declaring instance variables as private
         3. Providing accessor and mutator methods to view and modify
 
+        Even if a overloaded Constructer's paramter doesn't recieve a value of a instance variable, the programmer MUST provide a default value.
         
+        If no constructor is provided, JAVA will provide a default constructor.
+        --> int 0, double 0, other objects null
+
+
+        Javadoc Comments
+        @params, @author, @version
+
+        Precondition
+        - Comments for a method
+        - Conditions that must be met for the method to work
+        - Code in method is based on precondition being true
+        - precondition, postcondition
+        
+        Accessor Methods
+        - get methods
+        - must be public 
+        - return type should match
+        - camelCase
+        - No parameters
+
+        toString method 
+        - returns a string representation of the object
+
+        Look at Code below!
         */
     }
-
     public class Snack{
-
         // Private Instance Variables
         private String name;
         private int calories;
@@ -37,9 +60,9 @@ public class Unit5 {
             this.name = "";
             this.calories = 0;
         }
-
+    
         // Overloaded Constructor
-        public Snack(String n, int c){
+        public Snack(String n, int c){ // Since n,c is local, other methods cannot use it.
             this.name = n;
             this.calories = c;
         }
@@ -58,12 +81,26 @@ public class Unit5 {
             this.name = n;
         }
 
+        /**
+         * Method to set value of calories
+         * @param c - value to set calories
+         * Precondition: c must be greater than 0
+         * Postcondition: calories is set to c
+         */
         public void setCalories(int c){
             this.calories = c;
         }
 
         private Boolean canEat(){
             return(this.calories < 150);
+        }
+
+        // Snack cookie = new Snack("Oreo", 140);
+        // System.out.println(cookie);
+        // --> toString method is called automatically
+        public String toString(){ 
+            // This is printed
+            return "Name: " + this.name + " Calories: " + this.calories;
         }
     }
 }
