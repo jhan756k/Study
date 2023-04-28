8f27c21d395630bfebd27aa0b438ff9c9ec07afa
Jooney <jhan756k@gmail.com>
2023-04-28 17:27:21 +0900

    Unit7

--- APCSA Study/Unit7.java
+++ APCSA Study/Unit7.java
@@ -1,9 +1,20 @@
+import java.util.ArrayList;
 public class Unit7 {
     public static void main(String[] args) {
         /*
         7. ARRAYLIST
 
-        
+        Arrays are static in size (once initialized) - primitive data type
+        Arraylists are dynamic in size (can be changed) - object
+        - Arraylist is made with the array class.
+
+        import java.util.ArrayList;
+        ArrayList<Integer> num = new ArrayList<Integer>(n);
+        - n is the initial size of the arraylist (not required)
+        Arraylists can only store objects, so Wrapper classes are a workaround.
+
+        new ArrayList<E>(); where E is object type
         */
+        ArrayList<Boolean> lo = new ArrayList<Boolean>();
     }
 }
