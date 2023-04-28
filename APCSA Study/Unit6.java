e9983e47dcd75d96a7936e70c59f796f0e3b744e
Jooney <jhan756k@gmail.com>
2023-04-28 14:30:25 +0900

    unit6

--- APCSA Study/Unit6.java
+++ APCSA Study/Unit6.java
@@ -2,8 +2,12 @@ public class Unit6 {
     public static void main(String[] args) {
         /*
         6. ARRAY
-
         
+        you can create array of any object using [] keyword
+        Array length --> arr.length
+        String legth --> str.length()
+        
+        0 1 2 3 4
         */
     }
 }
