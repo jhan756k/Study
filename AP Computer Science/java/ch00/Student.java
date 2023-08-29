a54c90a9423b8d7fda536aea0c0b6ac22d2c29f2
Jooney <jhan756k@gmail.com>
2023-08-29 11:08:58 +0900

    apcsa hw

--- AP Computer Science/java/ch00/Student.java
+++ AP Computer Science/java/ch00/Student.java
@@ -7,14 +7,69 @@ public class Student {
     public Student() {
         myAge = 0;
         myName = "";
-        myGrades = new float[5];
         myHeight = 0.0f;
     }
 
-    public Student(int age, String name, float[] grades, float height) {
+    public Student(int age, String name, float height) {
         myAge = age;
         myName = name;
-        myGrades = grades;
         myHeight = height;
+    }
+
+    {
+        myGrades = new float[]{0.0f, 0.0f, 0.0f, 0.0f, 0.0f};
+    }
+
+    public String getGrade() {
+        String gString = "";
+        for (int i = 0; i < 5; i++) {
+            gString += myGrades[i] + " ";
+        }
+        return gString;
+    }
+
+    public float getHeight() {
+        return myHeight;
+    }
+
+    public int getAge() {
+        return myAge;
+    }
+
+    public String getName() {
+        return myName;
+    }
+
+    public void addGrade(float grade) {
+        boolean isAdded = false;
+        for (int i = 0; i < 5; i++) {
+            if (myGrades[i] == 0.0f) {
+                myGrades[i] = grade;
+                isAdded = true;
+                break;
+            }
+        }
+
+        if (!isAdded) {
+            System.out.println("You already added 5 grades.");
+        }
+    }
+
+    public float getAverage() {
+        float sum = 0.0f;
+        int pnum = 0;
+        float avg = 0.0f;
+        for (int i = 0; i < myGrades.length; i++) {
+            if (myGrades[i] != 0.0f) {
+                pnum++;
+                sum += myGrades[i];
+            }
+        }
+        avg = sum / pnum;
+        return avg;
+    }
+
+    public String toString() {
+        return "Name: " + getName() + "\nAge: " + getAge() + "\nHeight: " + getHeight() + "\nGrades: " + getGrade() + "\nAVG: " + getAverage();
     }
 }
--- AP Computer Science/java/ch00/StudentTest.java
+++ AP Computer Science/java/ch00/StudentTest.java
@@ -1,5 +1,13 @@
 public class StudentTest {
     public static void main(String[] args) {
-        
+        Student s1 = new Student(17, "오지훈", 180);
+
+        for (int i = 0; i < 5; i++) {
+            float randnum = (float)(Math.random() * 50 + 50);
+            s1.addGrade(randnum);
+        }
+
+        s1.getAverage();
+        System.out.println(s1.toString());
     }
 }
