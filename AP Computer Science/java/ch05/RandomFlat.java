6300d79ba5628ba8561184a3726352e8cb44c71c
Jooney <jhan756k@gmail.com>
2023-10-04 14:09:49 +0900

    overgob

--- AP Computer Science/java/ch05/RandomFlat.java
+++ AP Computer Science/java/ch05/RandomFlat.java
@@ -8,19 +8,23 @@ public class RandomFlat {
         int times = 1000000;
         float arr[] = new float[times];
         int start = 0;
-        int end = 2;
+        int end = 10;
+        int sd = 0;
 
-        for (int i = 0; i < times; i++){
-            arr[i] = start + (end-start)*r.nextFloat();
-            // System.out.println(arr[i]);
+        for (int i = 0; i < times; i++) {
+            arr[i] = start + (end - start) * r.nextFloat();
+            sd += arr[i];
         }
-
-        int[] count = new int[end-start+1];
-        for (int i = 0; i < times; i++){
-            count[(int)arr[i]-start]++;
-        }   
-        for (int i = 0; i < end-start+1; i++){
+        sd /= arr.length;
+        float s = 0;
+        int[] count = new int[end - start + 1];
+        for (int i = 0; i < times; i++) {
+            count[(int) arr[i] - start]++;
+            s += Math.pow(arr[i] - sd, 2);
+        }
+        for (int i = 0; i < end - start + 1; i++) {
             System.out.println(count[i]);
         }
+        System.out.println(s/arr.length);
     }
 }
