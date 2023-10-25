3a4331741ca67cca5d91adbb2d57831e0de66225
Jooney <jhan756k@gmail.com>
2023-10-25 14:30:12 +0900

    inverse

--- AP Computer Science/java/ch07/MyMatrix.java
+++ AP Computer Science/java/ch07/MyMatrix.java
@@ -42,24 +42,78 @@ public class MyMatrix {
     public MyMatrix addMatrix(MyMatrix m) {
         if (numRows != m.numRows || numCols != m.numCols) {
             System.out.println("Error: cannot add matrices of different sizes");
-            return;
+            return null;
         }
         for (int i = 0; i < numRows; i++) {
             for (int j = 0; j < numCols; j++) { 
                 myElements[i][j] += m.myElements[i][j];
             }
         }
+        return m;
     }
 
     public MyMatrix subMatrix(MyMatrix m) {
         if (numRows != m.numRows || numCols != m.numCols) {
             System.out.println("Error: cannot subtract matrices of different sizes");
-            return;
+            return null;
         }
         for (int i = 0; i < numRows; i++) {
             for (int j = 0; j < numCols; j++) { 
                 myElements[i][j] -= m.myElements[i][j];
             }
         }
+        return m;
     }
+
+    public MyMatrix multMatrix(MyMatrix m) {
+        if (numCols != m.numRows) {
+            System.out.println("Error: cannot multiply matrices of these sizes");
+            return null;
+        }
+        MyMatrix result = new MyMatrix(myName + " x " + m.myName, numRows, m.numCols, 0.0);
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < m.numCols; j++) {
+                for (int k = 0; k < numCols; k++) {
+                    result.myElements[i][j] += myElements[i][k] * m.myElements[k][j];
+                }
+            }
+        }
+        return result;
+    }
+
+    public double determinant() {
+        if (numRows != numCols) {
+            System.out.println("Error: cannot compute determinant of a non-square matrix");
+            return 0.0;
+        }
+        if (numRows == 1) {
+            return myElements[0][0];
+        }
+        else {
+            double det = 0.0;
+            for (int i = 0; i < numRows; i++) {
+                MyMatrix subMatrix = new MyMatrix(myName + " sub " + i, numRows - 1, numCols - 1, 0.0);
+                for (int j = 1; j < numRows; j++) {
+                    for (int k = 0; k < numRows; k++) {
+                        if (k < i) {
+                            subMatrix.myElements[j - 1][k] = myElements[j][k];
+                        }
+                        else if (k > i) {
+                            subMatrix.myElements[j - 1][k - 1] = myElements[j][k];
+                        }
+                    }
+                }
+                det += Math.pow(-1.0, i) * myElements[0][i] * subMatrix.determinant();
+            }
+            return det;
+        }
+    }
+
+    // public MyMatrix inverseMatrix() {
+    //     if (numRows != numCols) {
+    //         System.out.println("Error: cannot invert a non-square matrix");
+    //         return null;
+    //     }
+        
+    // }
 }
--- AP Computer Science/java/ch07/MyMatrixTest.java
+++ AP Computer Science/java/ch07/MyMatrixTest.java
@@ -1,11 +1,24 @@
 package ch07;
+import chn.util.*;
 
 public class MyMatrixTest {
-    public static void main(String[] args) {
-        MyMatrix m = new MyMatrix();
-        m.printMatrix();
+    public static void main(String[] args)   {
+        FileInput io = new FileInput("java\\ch07\\matrix.txt");
+        String name = io.readLine();
+        int rows = io.readInt();
+        int cols = io.readInt();
+        
 
-        MyMatrix m2 = new MyMatrix("m2", 3, 4, 1.0);
-        m2.printMatrix();
+        // MyMatrix A = new MyMatrix("A", 3, 4, 1.0);
+        // A.printMatrix();
+
+        // MyMatrix B = new MyMatrix("B", 4, 4, 2.0);
+        // B.printMatrix();
+
+        // MyMatrix x = A.multMatrix(B);
+        // x.printMatrix();
+
+        // MyMatrix sq = new MyMatrix("sq", 3, 3, 3.0);
+        // sq.printMatrix();
     }
 }
