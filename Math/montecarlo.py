2ec90c79dabe95590d8dc46ddf69952292269ca4
Jooney <jhan756k@gmail.com>
2024-05-20 21:32:59 +0900

    fi

--- Math/montecarlo.py
+++ Math/montecarlo.py
@@ -4,7 +4,9 @@ sum = 0.0
 pdf = 1.0 / ((4*math.pi)/3)
 
 for i in range(1, 10000001):
+  
   rand = random.random() * ((4*math.pi)/3)
   sum += math.sin(rand) / pdf
+  
   if i in [10, 100, 1000, 10000, 10000000]:
     print("n = " + str(i) + " : " + str(sum/i))
